63c6c37d5743aa9abb8e59b3f83b1c3d207b2cb0
Ajay <ajaychekavarck@gmail.com>
2025-11-02 02:38:32 +0530

    sticky poisoning bug fix

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -16,11 +16,12 @@ const Projects = () => {
       </div>
 
       <div className="pt-24">
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-24">
           {projectsData.map((project, index) => (
             <div
               id={`sticky-card-${index + 1}`}
               key={index}
+              style={{ top: `${index * 100}px` }}
               className="sticky-card w-full mx-auto max-w-2xl sticky"
             >
               <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">

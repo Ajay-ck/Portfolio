3689a8411273b1fc5d08ad98466cb5f6b7508de4
Ajay <ajaychekavarck@gmail.com>
2025-11-02 02:49:20 +0530

    fix

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -4,25 +4,24 @@ import ProjectCard from './project-card';
 const Projects = () => {
 
   return (
-    <div id='projects' className="relative z-50  my-12 lg:my-24">
-      <div className="sticky top-10">
-        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
+    <div id='projects' className="relative z-50 my-12 lg:my-24">
+      <div className="sticky top-10 z-10 bg-[#0d1224] mb-12">
+        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl opacity-30"></div>
         <div className="flex items-center justify-start relative">
-          <span className="bg-[#1a1443] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
+          <span className="bg-[#1a1443] absolute left-0 w-fit text-white px-5 py-3 text-xl rounded-md">
             PROJECTS
           </span>
           <span className="w-full h-[2px] bg-[#1a1443]"></span>
         </div>
       </div>
 
-      <div className="pt-24">
-        <div className="flex flex-col gap-24">
+      <div className="relative pt-12">
+        <div className="flex flex-col gap-[60vh]">
           {projectsData.map((project, index) => (
             <div
               id={`sticky-card-${index + 1}`}
               key={index}
-              style={{ top: `${index * 100}px` }}
-              className="sticky-card w-full mx-auto max-w-2xl sticky"
+              className="sticky top-32 w-full mx-auto max-w-2xl h-fit"
             >
               <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
                 <ProjectCard project={project} />
